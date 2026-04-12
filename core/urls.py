9534c5887e384cb1c87b02150a51d970786dfa21
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-12 17:08:11 +0100

    Add API documentation with Swagger

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import home
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('api/products/', include('products.urls')),
+]
+
+schema_view = get_schema_view(
+   openapi.Info(title="Ecommerce API", default_version='v1'),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
